214f5cc55de0c4df85d221da5aa667ae4de7efc6
luqin2007 <lq2007lq@hotmail.com>
2024-09-25 16:20:33 +0800

    c#

---
+++ _resources/codes/CSharp/out.cs
@@ -0,0 +1,15 @@
+﻿void outMethod(out int a, out object b) {
+    // ↓ out 必须在使用前对其赋值
+    a = 1;
+    b = new object();
+    //  1, System.Object
+    Console.WriteLine($"{a}, {b}");
+}
+   
+int a = 0;
+object b = new { name = "b" };
+//  0, { name = b }
+Console.WriteLine($"{a}, {b}");
+outMethod(out a, out b);
+//  1, System.Object
+Console.WriteLine($"{a}, {b}");
---
+++ _resources/codes/CSharp/ref.cs
@@ -0,0 +1,20 @@
+﻿void method(A a) {
+    a.X = 2;
+    a = new A { X = 3 };
+}
+
+void methodRef(ref A a) {
+    a.X = 2;
+    a = new A { X = 3 };
+}
+
+A a1 = new A { X = 1 };
+A a2 = new A { X = 1 };
+method(a1);
+methodRef(ref a2);
+Console.WriteLine(a1.X); // 3 a 是 a1 的引用
+Console.WriteLine(a2.X); // 2 a 是 a1
+
+class A {
+    public int X { get; set; }
+}
